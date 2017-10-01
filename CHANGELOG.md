d38acb1d210475a92819dcc952137f9619869d87
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2017-10-01 14:23:05 +0200

    Use luminance-windowing.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,49 +1,55 @@
-## 0.3.3
+## 0.4.0
+
+> Sunday, 1st of October, 2017
+
+- Use `luminance-windowing` to benefit from the types and functions defined in there.
+
+### 0.3.3
 
 > Saturday, 30th of September, 2017
 
 - Remove the `luminance` dependency as it’s not needed anymore.
 
-## 0.3.2
+### 0.3.2
 
 - Poll everything!
 
-## 0.3.1
+### 0.3.1
 
 - Support for `glfw-0.16`.
 
-# 0.3.0
+## 0.3.0
 
 - Removed `open_window` and moved its code into `Device::new`.
 - Enhanced the documentation.
 - Implemented Hi-DPI (tested on a Macbook Pro).
 - Removed `Device::width` and `Device::height` and replaced them with `Device::size`.
 
-# 0.2.0
+## 0.2.0
 
 - Changed the way events are handled. It doesn’t create an events thread anymore but instead exposes
   a polling interface. It’ll enhance performance and make mono-thread systems work, like Mac OSX.
 
-## 0.1.5
+### 0.1.5
 
 - Internal fix for Mac OSX.
 
-## 0.1.4
+### 0.1.4
 
 - Updated `luminance` dependencies.
 
-## 0.1.3
+### 0.1.3
 
 - Changed the trait bound from `Fn` to `FnOnce` on `Device::draw`.
 
-## 0.1.2
+### 0.1.2
 
 - Made `glfw::InitError` visible.
 
-# 0.1.1
+### 0.1.1
 
 - `Device::draw` added.
 
-# 0.1.0
+## 0.1.0
 
 - Initial revision.
--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,38 @@
-#![feature(conservative_impl_trait)]
-
 extern crate gl;
 extern crate glfw;
+extern crate luminance_windowing;
 
 use glfw::{Context, CursorMode, SwapInterval, Window, WindowMode};
 pub use glfw::{Action, InitError, Key, MouseButton, WindowEvent};
+pub use luminance_windowing::{Device, WindowDim, WindowOpt};
 use std::os::raw::c_void;
 use std::sync::mpsc::Receiver;
 
 /// Error that can be risen while creating a `Device` object.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub enum DeviceError {
+pub enum GLFWDeviceError {
   InitError(InitError),
   WindowCreationFailed,
   NoPrimaryMonitor,
   NoVideoMode
 }
 
-/// Dimension of the window to create.
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub enum WindowDim {
-  Windowed(u32, u32),
-  Fullscreen,
-  FullscreenRestricted(u32, u32)
-}
-
 /// Device object.
 ///
 /// Upon window and context creation, this type is used to add interaction and context handling.
-pub struct Device {
+pub struct GLFWDevice {
   /// Window.
   window: Window,
   /// Window events queue.
   events: Receiver<(f64, WindowEvent)>
 }
 
-impl Device {
-  /// Give the size of the framebuffer attached to the window.
-  pub fn size(&self) -> [u32; 2] {
-    let (w, h) = self.window.get_framebuffer_size();
-    [w as u32, h as u32]
-  }
+impl Device for GLFWDevice {
+  type Event = WindowEvent;
+  type Error = GLFWDeviceError;
 
-  /// Retrieve an iterator over any pulled events.
-  pub fn events<'a>(&'a mut self) -> impl Iterator<Item = (f64, WindowEvent)> + 'a {
-    self.window.glfw.poll_events();
-    glfw::flush_messages(&self.events)
-  }
-
-  /// Perform a draw. You should recall that function each time you want to draw a single frame to
-  /// the screen.
-  pub fn draw<F>(&mut self, f: F) where F: FnOnce() {
-    f();
-    self.window.swap_buffers();
-  }
-
-  /// Create a device and bootstrap a luminance environment that lives as long as thdevice
-  /// lives.
-  pub fn new(dim: WindowDim, title: &str, win_opt: WindowOpt) -> Result<Self, DeviceError> {
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).map_err(DeviceError::InitError)?;
+  fn new(dim: WindowDim, title: &str, win_opt: WindowOpt) -> Result<Self, Self::Error> {
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).map_err(GLFWDeviceError::InitError)?;
 
     // OpenGL hints
     glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
@@ -72,35 +46,39 @@ impl Device {
         glfw.create_window(w,
                            h,
                            title,
-                           WindowMode::Windowed).ok_or(DeviceError::WindowCreationFailed)?
+                           WindowMode::Windowed).ok_or(GLFWDeviceError::WindowCreationFailed)?
       },
       WindowDim::Fullscreen => {
         glfw.with_primary_monitor(|glfw, monitor| {
-          let monitor = monitor.ok_or(DeviceError::NoPrimaryMonitor)?;
-          let vmode = monitor.get_video_mode().ok_or(DeviceError::NoVideoMode)?;
+          let monitor = monitor.ok_or(GLFWDeviceError::NoPrimaryMonitor)?;
+          let vmode = monitor.get_video_mode().ok_or(GLFWDeviceError::NoVideoMode)?;
           let (w, h) = (vmode.width, vmode.height);
 
-          Ok(glfw.create_window(w,
-                                h,
-                                title,
-                                WindowMode::FullScreen(monitor)).ok_or(DeviceError::WindowCreationFailed)?)
+          Ok(glfw.create_window(
+              w,
+              h,
+              title,
+              WindowMode::FullScreen(monitor)
+              ).ok_or(GLFWDeviceError::WindowCreationFailed)?)
         })?
       },
       WindowDim::FullscreenRestricted(w, h) => {
         glfw.with_primary_monitor(|glfw, monitor| {
-          let monitor = monitor.ok_or(DeviceError::NoPrimaryMonitor)?;
+          let monitor = monitor.ok_or(GLFWDeviceError::NoPrimaryMonitor)?;
 
-          Ok(glfw.create_window(w,
-                                h,
-                                title,
-                                WindowMode::FullScreen(monitor)).ok_or(DeviceError::WindowCreationFailed)?)
+          Ok(glfw.create_window(
+              w,
+              h,
+              title,
+              WindowMode::FullScreen(monitor)
+              ).ok_or(GLFWDeviceError::WindowCreationFailed)?)
         })?
       }
     };
 
     window.make_current();
 
-    if win_opt.hide_cursor {
+    if win_opt.is_cursor_hidden() {
       window.set_cursor_mode(CursorMode::Disabled);
     }
 
@@ -110,36 +88,24 @@ impl Device {
     // init OpenGL
     gl::load_with(|s| window.get_proc_address(s) as *const c_void);
 
-    Ok(Device {
+    Ok(GLFWDevice {
       window: window,
       events: events
     })
   }
-}
 
-/// Different window options.
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub struct WindowOpt {
-  hide_cursor: bool
-}
-
-impl Default for WindowOpt {
-  fn default() -> Self {
-    WindowOpt {
-      hide_cursor: false
-    }
-  }
-}
-
-impl WindowOpt {
-  /// Hide or unhide the cursor. Default to `false`.
-  #[inline]
-  pub fn hide_cursor(self, hide: bool) -> Self {
-    WindowOpt { hide_cursor: hide, ..self }
+  fn size(&self) -> [u32; 2] {
+    let (w, h) = self.window.get_framebuffer_size();
+    [w as u32, h as u32]
   }
 
-  #[inline]
-  pub fn is_cursor_hidden(&self) -> bool {
-    self.hide_cursor
+  fn events<'a>(&'a mut self) -> Box<Iterator<Item = Self::Event> + 'a> {
+    self.window.glfw.poll_events();
+    Box::new(glfw::flush_messages(&self.events).map(|(_, e)| e))
+  }
+
+  fn draw<F>(&mut self, f: F) where F: FnOnce() {
+    f();
+    self.window.swap_buffers();
   }
 }
