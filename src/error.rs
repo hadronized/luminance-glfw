0666f3cab4efe8aa5990cbef36616c5293b4e742
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2018-08-15 01:04:38 +0200

    Remove the std::error::Error implementor.

--- src/error.rs
+++ src/error.rs
@@ -2,7 +2,6 @@
 pub use glfw::InitError;
 pub use luminance::state::StateQueryError;
 
-use std::error::Error;
 use std::fmt;
 
 /// Error that can be risen while creating a surface.
@@ -28,27 +27,12 @@ pub enum GlfwSurfaceError {
 // TODO: better implementation
 impl fmt::Display for GlfwSurfaceError {
   fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-    f.write_str(self.description())
-  }
-}
-
-impl Error for GlfwSurfaceError {
-  fn description(&self) -> &str {
     match *self {
-      GlfwSurfaceError::InitError(_) => "initialization error",
-      GlfwSurfaceError::WindowCreationFailed => "failed to create window",
-      GlfwSurfaceError::NoPrimaryMonitor => "no primary monitor",
-      GlfwSurfaceError::NoVideoMode => "no video mode",
-      GlfwSurfaceError::GraphicsStateError(_) => "failed to get graphics state",
-    }
-  }
-
-  fn cause(&self) -> Option<&Error> {
-    match *self {
-      GlfwSurfaceError::InitError(ref e) => Some(e),
-      GlfwSurfaceError::GraphicsStateError(ref e) => Some(e),
-      _ => None
+      GlfwSurfaceError::InitError(ref e) => write!(f, "initialization error: {}", e),
+      GlfwSurfaceError::WindowCreationFailed => f.write_str("failed to create window"),
+      GlfwSurfaceError::NoPrimaryMonitor => f.write_str("no primary monitor"),
+      GlfwSurfaceError::NoVideoMode => f.write_str("no video mode"),
+      GlfwSurfaceError::GraphicsStateError(ref e) => write!(f, "failed to get graphics state: {}", e),
     }
   }
 }
-
